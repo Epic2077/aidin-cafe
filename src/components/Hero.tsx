be7e58312c0d4ee7329eaa98d4670d579a335412
Ashkan <the.epic.2077@gmail.com>
2025-07-11 16:59:16 +0330

    feat(hero): smal changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <div className='bg-[url("/hero-star.png")] w-[50%] aspect-square mx-auto bg-center bg-contain bg-no-repeat flex justify-center items-center'>
       <div className="relative">
-        <div className="absolute top-0 left-0 ml-[-50px] ">
+        <div className="absolute top-0 left-0 ml-[-50px] mt-[-20px]">
           <Image src="/bird.png" alt="bird" width={100} height={100} />
         </div>
-        <h1 className="text-center text-9xl leading-44 mt-[-40px]">
+        <h1 className="text-center text-9xl leading-44 mt-[-50px]">
           کافه و عمارت
           <br />
           <span className="text-[#C42136]">آیدین</span>
