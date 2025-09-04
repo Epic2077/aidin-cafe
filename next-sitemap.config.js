d8488300bb09c80ca47f93e3a1406c6b30f5779a
Ashkan <the.epic.2077@gmail.com>
2025-09-04 22:51:44 +0330

    analytics

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://aidin-cafe.vercel.app", // your domain
+  generateRobotsTxt: true, // will generate robots.txt
+  sitemapSize: 7000,
+};
