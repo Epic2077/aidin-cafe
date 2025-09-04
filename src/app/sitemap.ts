122bad184247563a00c99d7018be4cad9c7db971
Ashkan <the.epic.2077@gmail.com>
2025-09-04 22:55:37 +0330

    analytics

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,26 +5,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://aidin-cafe.vercel.app/",
       lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    },
-    {
-      url: "https://aidin-cafe.vercel.app//menu",
-      lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://aidin-cafe.vercel.app//about",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
-    {
-      url: "https://aidin-cafe.vercel.app//contact",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
+      priority: 1,
     },
   ];
 }
