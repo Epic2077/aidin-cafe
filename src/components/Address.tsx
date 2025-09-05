87014772ee127540d39f9a799eca0be7bf1c8bec
Ashkan <the.epic.2077@gmail.com>
2025-09-05 11:45:56 +0330

    Changes

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -9,9 +9,9 @@ const translations = {
     addressLabel: "آدرس عمارت:",
     address: "خیابان لطفعلی زند - مسجد نصیر الملک - عمارت آیدین",
     phoneLabel: "شماره تلفن:",
-    phone: "",
+    phone: "+98 9334896083",
     instagramLabel: "اینستاگرام:",
-    instagram: "",
+    instagram: "emarat_aidin",
     scheduleLabel: "زمانبندی کافه:",
     schedule: "",
   },
@@ -20,9 +20,9 @@ const translations = {
     addressLabel: "Emarat Address:",
     address: "Lotfali Zand St - Nasir al-Mulk Mosque - Aidin Emarat",
     phoneLabel: "Phone:",
-    phone: "",
+    phone: "+98 9334896083",
     instagramLabel: "Instagram:",
-    instagram: "",
+    instagram: "emarat_aidin",
     scheduleLabel: "Cafe Schedule:",
     schedule: "",
   },
@@ -41,13 +41,13 @@ const Address = () => {
         {t.title}
       </h2>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d250.48325903841112!2d52.54906272709848!3d29.608400652990674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sshiraz!5e0!3m2!1sen!2sde!4v1752529292694!5m2!1sen!2sde"
-        width="100%"
-        height="300"
-        allowFullScreen
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d433.6047906435634!2d52.54908062023745!3d29.60838579931941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fb20d002794d793%3A0x7523d388e0baf323!2sAidin%20cafe!5e0!3m2!1sen!2s!4v1757059533631!5m2!1sen!2s"
+        width="600"
+        height="450"
+        style="border:0;"
+        allowfullscreen=""
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        className="rounded-2xl w-full h-[300px] sm:h-[400px] md:h-[450px] lg:h-[500px]"
+        referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
       <div className="w-full mt-10 space-y-10 sm:space-y-7 md:space-y-8 sm:mt-10 md:mt-10 px-2 sm:px-4 md:px-6">
         <p
@@ -68,6 +68,7 @@ const Address = () => {
           <span className="pl-2 text-[#13888C] font-bold">{t.phoneLabel}</span>{" "}
           {t.phone}
         </p>
+        <a href="https://www.instagram.com/emarat_aidin"></a>
         <p
           className={`text-sm sm:text-base md:text-lg lg:text-xl text-center ${
             isFa ? "lg:text-right" : "lg:text-left"
@@ -77,16 +78,6 @@ const Address = () => {
             {t.instagramLabel}
           </span>{" "}
           {t.instagram}
-        </p>
-        <p
-          className={`text-sm sm:text-base md:text-lg lg:text-xl text-center ${
-            isFa ? "lg:text-right" : "lg:text-left"
-          } mb-5`}
-        >
-          <span className="pl-2 text-[#13888C] font-bold">
-            {t.scheduleLabel}
-          </span>{" "}
-          {t.schedule}
         </p>
       </div>
     </div>
