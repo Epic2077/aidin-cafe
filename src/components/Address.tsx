9e99f79325a762c2b5939cc33bfae4003854ef84
Ashkan <the.epic.2077@gmail.com>
2025-09-05 11:55:18 +0330

    Changes

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -42,7 +42,7 @@ const Address = () => {
       </h2>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d433.6047906435634!2d52.54908062023745!3d29.60838579931941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fb20d002794d793%3A0x7523d388e0baf323!2sAidin%20cafe!5e0!3m2!1sen!2s!4v1757059533631!5m2!1sen!2s"
-        width="600"
+        width="80%"
         height="450"
         loading="lazy"
       ></iframe>
